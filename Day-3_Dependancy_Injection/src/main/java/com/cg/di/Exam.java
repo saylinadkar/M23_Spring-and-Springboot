6ccd31ec416e31833cd030c0b35579325cacddf8
saylinadkar <101880800+saylinadkar@users.noreply.github.com>
2022-09-27 21:26:42 +0530

    first commit

---
+++ Day-3_Dependancy_Injection/src/main/java/com/cg/di/Exam.java
@@ -0,0 +1,25 @@
+package com.cg.di;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+
+
+public class Exam {
+
+	public static void main(String[] args) {
+		/*Student s=new Student();
+		s.setStudentName("Sayli");
+		s.display();*/
+		
+		ApplicationContext c=new ClassPathXmlApplicationContext("beans.xml");
+		Student s=c.getBean("s",Student.class);
+		s.display();
+		
+		Student s1=c.getBean("s1",Student.class);
+		s1.display();
+	}
+
+}
